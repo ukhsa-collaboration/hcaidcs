7678078c45b7c92b805764bbfc09eb216c6d6672
Simon Thelwall <simon.thelwall@phe.gov.uk>
2016-06-17 14:54:12 +0100

    Minor fix for ordered_fin_qtr so that Januaries are not placed together.

--- R/ordered_fin_qtr.R
+++ R/ordered_fin_qtr.R
@@ -20,6 +20,18 @@
 #' dat <- ordered_fin_qtr(dat, "sample_fq")
 #' class(dat$fin_qtr_ftr)
 #' levels(dat$fin_qtr_ftr)
+#'
+#' dat <- data.frame(sample_date = c(lubridate::dmy("01/01/2001"),
+#'    lubridate::dmy("01/04/2001"),
+#'    lubridate::dmy("01/07/2001"),
+#'    lubridate::dmy("01/10/2001"),
+#'    lubridate::dmy("01/01/2002")
+#'    ))
+#' dat$sample_fq <- fq_long(dat$sample_date)
+#' dat <- ordered_fin_qtr(dat, "sample_fq")
+#' class(dat$fin_qtr_ftr)
+#' levels(dat$fin_qtr_ftr)
+#'
 #' @importFrom magrittr "%>%"
 #' @export
 
@@ -28,8 +40,12 @@ ordered_fin_qtr <- function(data, quarter_in_long_format){
     stop("lubridate is needed for this function to work. Please install it.",
          call. = FALSE)
   }
-  z <- data %>% dplyr::mutate_(temp_year = lazyeval::interp(~substr(var, nchar(var)-4, nchar(var)), var = as.name(quarter_in_long_format) ),
-                        text_qtr = lazyeval::interp(~substr(var, 1, nchar(var)-5), var = as.name(quarter_in_long_format) )) %>%
+  z <- data %>% dplyr::mutate_(temp_year = lazyeval::interp(~substr(var, nchar(var)-4, nchar(var)),
+                                                            var = as.name(quarter_in_long_format) ),
+                               text_qtr = lazyeval::interp(~substr(var, 1, nchar(var)-5),
+                                                           var = as.name(quarter_in_long_format) )) %>%
+    dplyr::mutate(temp_year = as.numeric(temp_year),
+      temp_year = ifelse(text_qtr == "January to March", temp_year-1, temp_year)) %>%
     dplyr::mutate(text_qtr2 = factor(text_qtr, levels = c("April to June",
                                                    "July to September",
                                                    "October to December",
