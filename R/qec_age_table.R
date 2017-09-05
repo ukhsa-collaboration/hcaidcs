67982613db998cab0fd170fce037a54fbb1c521b
Simon Thelwall <simon.thelwall@phe.gov.uk>
2017-09-05 13:42:08 +0100

    Fixed problem with qec_age_table, adapted so that it uses dplyr enquo rather than old lazyeval

--- R/qec_age_table.R
+++ R/qec_age_table.R
@@ -3,17 +3,17 @@
 #' Produces rates (per 100,000) by age group for the QEC.
 #' It's **really** important that the value for the denominator is the same across all records for the same age group
 #'
-#' @param data A data frame
+#' @param df A data frame
 #' @param age_var A variable giving the age group for the record
 #' @param denom A variable giving the denominator for the age **group**
 #' @return a tibble containing the aggregated data
 #' @examples
 #' dat <- data.frame(age_group = c(1, 1, 1, 2, 2, 2, 2),
-#'                   denom = c(500000,500000,500000, 600000, 600000, 600000, 600000))
-#' qec_age_table(data = dat, age_var = "age_group", denom = "denom")
+#'                   denominator = c(500000,500000,500000, 600000, 600000, 600000, 600000))
+#' qec_age_table(df = dat, age_var = age_group, denom = denominator)
 #' @export
 
-qec_age_table <- function(data, age_var, denom){
+qec_age_table <- function(df, age_var, denom){
   if (!requireNamespace("dplyr", quietly = TRUE)) {
     stop("dplyr needed for this function to work. Please install it.",
          call. = FALSE)
@@ -22,11 +22,16 @@ qec_age_table <- function(data, age_var, denom){
     stop("dplyr needed for this function to work. Please install it.",
          call. = FALSE)
   }
-  z <- data %>%
-    dplyr::group_by_(as.name(age_var)) %>%
-    dplyr::summarise_(count = "dplyr::n()",
-               denom = lazyeval::interp(~mean(var), var = as.name(denom))
-    ) %>%
-    dplyr::mutate(rate_per_100k = (count / denom) * 100000)
+  group_var <- dplyr::enquo(age_var)
+  denom_var <- dplyr::enquo(denom)
+
+  z <- df %>%
+    dplyr::group_by(!!group_var) %>%
+    dplyr::summarise(
+      # a_count = "dplyr::n()",
+      a_count = n(),
+      denom = min(!!denom_var)
+               )  %>%
+    dplyr::mutate(rate_per_100k = (a_count / denom) * 100000)
   return(z)
 }
