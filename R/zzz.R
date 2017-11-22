d204e52e9fcf31c2693761eefa031a89a34bdeb1
Simon Thelwall <simon.thelwall@phe.gov.uk>
2017-11-22 11:03:56 +0000

    Squashed commit of the following:
    commit 1fb993fe00d4e128d146471ccada3333036f8b16
    Author: Simon Thelwall <simon.thelwall@phe.gov.uk>
    Date:   Wed Nov 22 11:00:21 2017 +0000
    
        Added zzz.R file to display message on load

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Thanks for using the HCAIDCS package.
+                        Please note that the apportion_prior_healthcare function is still experimental and has not been validated by a second team member or against the DCS.")
+}
