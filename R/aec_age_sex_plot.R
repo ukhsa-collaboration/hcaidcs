ukhsa-collaboration/hcaidcs

commit 45bd7e7d197d7291afe46b4c9e4197d893e605f6

Fix apportion_prior_healthcare + add function to plot changes in age/sex
Minor change to apportion_prior_healthcare for edge cases where there
was contradictory info in admission in past 3 months and admission in
past 12 weeks.

Edited aec_age_trend_pc so that it includes ggrepel for labels and so
that it is over two columns and one row, rather than one column and two
rows.

Added function aec_age_trend_rate_pc_change to show percentage change
in age/sex rates as this is more informative than pc cases by age group.

DESCRIPTION
@@ -28,7 +28,8 @@ Imports:
     tidyr,
     cowplot,
     assertthat,
-    rlang
+    rlang,
+    ggrepel
 Collate:
     'aec_age_sex_plot.R'
     'aec_age_trends_plots.R'

commit a7e9d6ff190959dcca1f6f7c89c14ee22857f8eb

Added Fixed some bugs with apportion_prior_hc
Commit 45bd7e7d197d7291afe46b4c9e4197d893e605f6 introduced a bug
by which NAs were not handled in edge cases where contradictory
information was provided in prior exposure fields.

Removed viridis dependency (github issue 8)

Mostly fixed issues that need cleaning up before release (github issue 1)
These were all documentation issues.

DESCRIPTION
@@ -23,7 +23,6 @@ Imports:
     magrittr,
     evaluate,
     ggplot2,
-    viridis,
     scales,
     tidyr,
     cowplot,

NAMESPACE
@@ -3,6 +3,7 @@
 export(aec_age_sex_plot)
 export(aec_age_trend_pc)
 export(aec_age_trend_rate)
+export(aec_age_trend_rate_pc_change)
 export(aec_create_folder_structure)
 export(aec_ec_source_plot)
 export(aec_inpatient)
@@ -12,6 +13,7 @@ export(ann_tab_ccg_names)
 export(ann_tab_onset_annual)
 export(ann_tab_onset_qtrly)
 export(ann_tab_pir_org_type)
+export(ann_tab_pir_quarterly)
 export(apportion)
 export(apportion_prior_healthcare)
 export(area_team_names)
@@ -77,6 +79,7 @@ export(time_period2fyear)
 export(time_to_onset)
 export(up_or_down)
 export(update_independent)
+importFrom(ggrepel,geom_text_repel)
 importFrom(lubridate,"%m+%")
 importFrom(lubridate,"%m-%")
 importFrom(magrittr,"%>%")

R/aec_age_sex_plot.R
@@ -62,8 +62,6 @@ aec_age_sex_plot <- function(x, collection = NULL){
                                 )
       ) +
       ggplot2::scale_y_continuous("Rate, per 100,000 population") +
-      # viridis::scale_fill_viridis("Sex", discrete = TRUE, option = "viridis",
-      #                             begin = 0.5, end = 0) +
       ggplot2::scale_fill_manual("Sex", values = c("#9ECAE1", "#3182BD")) +
       ggplot2::theme(#legend.position = c(0.9, 1), legend.justification = c(1,1),
         legend.position = "bottom",
@@ -84,8 +82,6 @@ aec_age_sex_plot <- function(x, collection = NULL){
                                   )
         ) +
         ggplot2::scale_y_continuous("Rate, per 100,000 population") +
-        # viridis::scale_fill_viridis("Sex", discrete = TRUE, option = "viridis",
-        #                             begin = 0.5, end = 0) +
         ggplot2::scale_fill_manual("Sex", values = c("#9ECAE1", "#3182BD")) +
         ggplot2::theme(#legend.position = c(0.9,1),
           legend.position = "bottom",

R/aec_age_trends_plots.R
@@ -153,6 +153,7 @@ aec_age_trend_rate <- function(collection, data, x, y, sex, group,log_scale = FA
 #' Plot the change in rate as a percentage from first year of surveillance.
 #'
 #' @param data a dataframe containing the following variables: fyear6, age_group_new, sex, rate
+#' @param collection A string giving either "CDI" or "bacteraemia". This determines the age groups
 #' @return A ggplot2 object
 #' @seealso \code{\link{aec_age_trend_pc}}
 #' @seealso \code{\link{aec_age_trend_rate}}

R/aec_subregion_plot.R
@@ -39,10 +39,6 @@ aec_subregion_plot <- function(x){
     stop("ggplot2 needed for this function to work. Please install it.",
          call. = FALSE)
   }
-  if (!requireNamespace("viridis", quietly = TRUE)) {
-    stop("The package *viridis* needed for this function to work. Please install it.",
-         call. = FALSE)
-  }
   ggplot2::ggplot(data = x,
                   ggplot2::aes(x = long, y = lat, group = group,
                        fill = rate,

R/ann_tab_onset.R
@@ -2,7 +2,19 @@
 #'
 #' Prepare quarterly MRSA PIR tables
 #'
+#' @param data  A data frame
+#' @param timeperiod A string giving the time period
+#' @param org_code A string giving the organisation code
+#' @param denominator A numeric variable giving the denominator (popn or bed days)
+#' @param total Count of total cases
+#' @param pir_trust_assigned Count of PIR trust-assigned cases
+#' @param pir_ccg_assigned Count of PIR CCG-assigned cases
+#' @param third_party Count of PIR third-party-assigned cases
+#' @param org_type String giving trust or CCG
+#' @return A data frame spread into format for wide tables.
+#' @export
 #' @examples
+#'
 #'
 #' pir_qtr_testdat <- data.frame(stringsAsFactors=FALSE,
 #'     time_period = c("April - June 2013", "July - September 2013",

R/apportion_prior_healthcare.R
@@ -141,8 +141,12 @@ apportion_prior_healthcare <- function(patient_location, patient_category,
                             ifelse(hoha == 0 & coha == 0 & coia == 0 & coca == 1, "coca", NA)
                      )))
   z <- ifelse(adm_3_mo == "don't know" & !is.na(adm_3_mo) & z != "hoha", "unknown_3_mo", z)
-  z <- ifelse(adm_3_mo == "no" & adm_4_weeks == "no" & adm_12_weeks == "yes", "coia", z)
-  z <- ifelse(adm_3_mo == "no" & adm_4_weeks == "yes" & adm_12_weeks == "yes", "coha", z)
+  z <- ifelse((adm_3_mo == "no"  & !is.na(adm_3_mo)) &
+                (adm_4_weeks == "no"  & !is.na(adm_4_weeks)) &
+                (adm_12_weeks == "yes" & !is.na(adm_4_weeks)), "coia", z)
+  z <- ifelse((adm_3_mo == "no"  & !is.na(adm_3_mo)) &
+                (adm_4_weeks == "yes"  & !is.na(adm_4_weeks)) &
+                (adm_12_weeks == "yes" & !is.na(adm_12_weeks)), "coha", z)
   z <- ifelse(z != "hoha" & is.na(adm_3_mo) & is.na(adm_4_weeks) &
                 is.na(adm_12_weeks), "all_blank", z)
   return(z)

R/data.R
@@ -99,8 +99,7 @@
 #'  \item{finalpirassignedorganisation}{string}
 #'  \item{patient_category}{string}
 #' }
-
-"cdi_prior_hc_data"
+"assignment_data"
 
 #' Data for testing prior healthcare exposure monthly table
 #'
@@ -121,11 +120,12 @@
 #'  \item{coha}{Count of coha cases}
 #'  \item{coia}{Count of coia cases}
 #'  \item{coca}{Count of coca cases}
+#'  \item{phc_all_blank}{Count of cases for which all prior health care fields are all blank}
+#'  \item{phc_unknown_3_mo}{Count of cases for which the three-month prior health care fields is blank}
 #'  \item{fmonth}{Numeric financial year month (i.e. 1 = April)}
 #'  \item{month_string}{String cal month}
 #' }
-
-"assignment_data"
+"cdi_prior_hc_data"
 
 #' Data for testing assignement algorithm
 #'
@@ -135,10 +135,10 @@
 #'  \item{pir_case_status}{string}
 #'  \item{assignment_method_code}{Numeric giving assignment method}
 #'  \item{patient_location}{string}
+#'  \item{patient_category}{string}
 #'  \item{provisional_organisation_name}{string}
 #'  \item{final_pir_assigned_organisation_type}{string}
-#'  \item{patient_category}{string}
-#'  \item{id}{numeric}
+#'  \item{id}{A numeric variable giving an anonymous HCAIDCS case id}
 #' }
 "assignment_data2"
 

docs/reference/aec_age_trend_rate.html
@@ -153,7 +153,8 @@ See <code><a href='age_trends_data.html'>age_trends_data</a></code> for example 
     
     <h2 class="hasAnchor" id="see-also"><a class="anchor" href="#see-also"></a>See also</h2>
 
-    <div class='dont-index'><p><code><a href='aec_age_trend_pc.html'>aec_age_trend_pc</a></code></p></div>
+    <div class='dont-index'><p><code><a href='aec_age_trend_pc.html'>aec_age_trend_pc</a></code></p>
+<p><code><a href='aec_age_trend_rate_pc_change.html'>aec_age_trend_rate_pc_change</a></code></p></div>
     
 
     <h2 class="hasAnchor" id="examples"><a class="anchor" href="#examples"></a>Examples</h2>

docs/reference/aec_age_trend_rate_pc_change.html
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Data for testing assignement algorithm — assignment_data • hcaidcs</title>
+<title>aec_age_trend_rate_pc_change — aec_age_trend_rate_pc_change • hcaidcs</title>
 
 <!-- jquery -->
 <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha384-nrOSfDHtoPMzJHjVTdCopGqIqeYETSXhZDFyniQ8ZHcVy08QesyHcnOUpMpqnmWq" crossorigin="anonymous"></script>
@@ -28,9 +28,9 @@
 
 
 
-<meta property="og:title" content="Data for testing assignement algorithm — assignment_data" />
+<meta property="og:title" content="aec_age_trend_rate_pc_change — aec_age_trend_rate_pc_change" />
 
-<meta property="og:description" content="A sample data set for testing the assignment_algorithm function." />
+<meta property="og:description" content="Plot the change in rate as a percentage from first year of surveillance." />
 <meta name="twitter:card" content="summary" />
 
 
@@ -97,38 +97,58 @@
 <div class="row">
   <div class="col-md-9 contents">
     <div class="page-header">
-    <h1>Data for testing assignement algorithm</h1>
-    <small class="dont-index">Source: <a href='https://github.com/PublicHealthEngland/hcaidcs/blob/master/R/data.R'><code>R/data.R</code></a></small>
-    <div class="hidden name"><code>assignment_data.Rd</code></div>
+    <h1>aec_age_trend_rate_pc_change</h1>
+    <small class="dont-index">Source: <a href='https://github.com/PublicHealthEngland/hcaidcs/blob/master/R/aec_age_trends_plots.R'><code>R/aec_age_trends_plots.R</code></a></small>
+    <div class="hidden name"><code>aec_age_trend_rate_pc_change.Rd</code></div>
     </div>
 
     <div class="ref-description">
     
-    <p>A sample data set for testing the assignment_algorithm function.</p>
+    <p>Plot the change in rate as a percentage from first year of surveillance.</p>
     
     </div>
 
-    <pre class="usage"><span class='no'>assignment_data</span></pre>
-        
-    <h2 class="hasAnchor" id="format"><a class="anchor" href="#format"></a>Format</h2>
+    <pre class="usage"><span class='fu'>aec_age_trend_rate_pc_change</span>(<span class='no'>data</span>, <span class='no'>collection</span>)</pre>
+    
+    <h2 class="hasAnchor" id="arguments"><a class="anchor" href="#arguments"></a>Arguments</h2>
+    <table class="ref-arguments">
+    <colgroup><col class="name" /><col class="desc" /></colgroup>
+    <tr>
+      <th>data</th>
+      <td><p>a dataframe containing the following variables: fyear6, age_group_new, sex, rate</p></td>
+    </tr>
+    <tr>
+      <th>collection</th>
+      <td><p>A string giving either "CDI" or "bacteraemia". This determines the age groups</p></td>
+    </tr>
+    </table>
+    
+    <h2 class="hasAnchor" id="value"><a class="anchor" href="#value"></a>Value</h2>
 
-    <p>A data frame with 14 rows of 6 variables:</p><dl class='dl-horizontal'>
- <dt>pircasestatus</dt><dd><p>string</p></dd>
- <dt>assignmentmethodcode</dt><dd><p>Numeric giving assignment method</p></dd>
- <dt>patientlocation</dt><dd><p>string</p></dd>
- <dt>provisionalorganisationname</dt><dd><p>string</p></dd>
- <dt>finalpirassignedorganisation</dt><dd><p>string</p></dd>
- <dt>patient_category</dt><dd><p>string</p></dd>
-</dl>
+    <p>A ggplot2 object</p>
+    
+    <h2 class="hasAnchor" id="see-also"><a class="anchor" href="#see-also"></a>See also</h2>
+
+    <div class='dont-index'><p><code><a href='aec_age_trend_pc.html'>aec_age_trend_pc</a></code></p>
+<p><code><a href='aec_age_trend_rate.html'>aec_age_trend_rate</a></code></p></div>
     
 
+    <h2 class="hasAnchor" id="examples"><a class="anchor" href="#examples"></a>Examples</h2>
+    <pre class="examples"><div class='input'><span class='fu'>data</span>(<span class='no'>age_trends_data</span>)
+<span class='no'>p</span> <span class='kw'>&lt;-</span> <span class='fu'>aec_age_trend_rate_pc_change</span>(<span class='kw'>data</span> <span class='kw'>=</span> <span class='no'>age_trends_data</span>, <span class='kw'>collection</span> <span class='kw'>=</span> <span class='st'>"CDI"</span>)
+<span class='no'>p</span></div><div class='img'><img src='aec_age_trend_rate_pc_change-1.png' alt='' width='700' height='433' /></div></pre>
   </div>
   <div class="col-md-3 hidden-xs hidden-sm" id="sidebar">
     <h2>Contents</h2>
     <ul class="nav nav-pills nav-stacked">
+      <li><a href="#arguments">Arguments</a></li>
       
-      <li><a href="#format">Format</a></li>
-          </ul>
+      <li><a href="#value">Value</a></li>
+
+      <li><a href="#see-also">See also</a></li>
+      
+      <li><a href="#examples">Examples</a></li>
+    </ul>
 
   </div>
 </div>

docs/reference/ann_tab_pir_quarterly.html
@@ -110,10 +110,56 @@
 
     <pre class="usage"><span class='fu'>ann_tab_pir_quarterly</span>(<span class='no'>data</span>, <span class='no'>timeperiod</span>, <span class='no'>org_code</span>, <span class='no'>denominator</span>, <span class='no'>total</span>,
   <span class='no'>pir_trust_assigned</span>, <span class='no'>pir_ccg_assigned</span>, <span class='no'>third_party</span>, <span class='no'>org_type</span>)</pre>
-        
+    
+    <h2 class="hasAnchor" id="arguments"><a class="anchor" href="#arguments"></a>Arguments</h2>
+    <table class="ref-arguments">
+    <colgroup><col class="name" /><col class="desc" /></colgroup>
+    <tr>
+      <th>data</th>
+      <td><p>A data frame</p></td>
+    </tr>
+    <tr>
+      <th>timeperiod</th>
+      <td><p>A string giving the time period</p></td>
+    </tr>
+    <tr>
+      <th>org_code</th>
+      <td><p>A string giving the organisation code</p></td>
+    </tr>
+    <tr>
+      <th>denominator</th>
+      <td><p>A numeric variable giving the denominator (popn or bed days)</p></td>
+    </tr>
+    <tr>
+      <th>total</th>
+      <td><p>Count of total cases</p></td>
+    </tr>
+    <tr>
+      <th>pir_trust_assigned</th>
+      <td><p>Count of PIR trust-assigned cases</p></td>
+    </tr>
+    <tr>
+      <th>pir_ccg_assigned</th>
+      <td><p>Count of PIR CCG-assigned cases</p></td>
+    </tr>
+    <tr>
+      <th>third_party</th>
+      <td><p>Count of PIR third-party-assigned cases</p></td>
+    </tr>
+    <tr>
+      <th>org_type</th>
+      <td><p>String giving trust or CCG</p></td>
+    </tr>
+    </table>
+    
+    <h2 class="hasAnchor" id="value"><a class="anchor" href="#value"></a>Value</h2>
+
+    <p>A data frame spread into format for wide tables.</p>
+    
 
     <h2 class="hasAnchor" id="examples"><a class="anchor" href="#examples"></a>Examples</h2>
     <pre class="examples"><div class='input'>
+
 <span class='no'>pir_qtr_testdat</span> <span class='kw'>&lt;-</span> <span class='fu'>data.frame</span>(<span class='kw'>stringsAsFactors</span><span class='kw'>=</span><span class='fl'>FALSE</span>,
     <span class='kw'>time_period</span> <span class='kw'>=</span> <span class='fu'>c</span>(<span class='st'>"April - June 2013"</span>, <span class='st'>"July - September 2013"</span>,
                     <span class='st'>"October - December 2013"</span>,
@@ -135,7 +181,10 @@
   <div class="col-md-3 hidden-xs hidden-sm" id="sidebar">
     <h2>Contents</h2>
     <ul class="nav nav-pills nav-stacked">
-            
+      <li><a href="#arguments">Arguments</a></li>
+      
+      <li><a href="#value">Value</a></li>
+      
       <li><a href="#examples">Examples</a></li>
     </ul>
 

docs/reference/assignment_data2.html
@@ -116,10 +116,10 @@
  <dt>pir_case_status</dt><dd><p>string</p></dd>
  <dt>assignment_method_code</dt><dd><p>Numeric giving assignment method</p></dd>
  <dt>patient_location</dt><dd><p>string</p></dd>
+ <dt>patient_category</dt><dd><p>string</p></dd>
  <dt>provisional_organisation_name</dt><dd><p>string</p></dd>
  <dt>final_pir_assigned_organisation_type</dt><dd><p>string</p></dd>
- <dt>patient_category</dt><dd><p>string</p></dd>
- <dt>id</dt><dd><p>numeric</p></dd>
+ <dt>id</dt><dd><p>A numeric variable giving an anonymous HCAIDCS case id</p></dd>
 </dl>
     
 

docs/reference/cdi_prior_hc_data.html
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Data for testing assignement algorithm — cdi_prior_hc_data • hcaidcs</title>
+<title>Data for testing prior healthcare exposure monthly table — cdi_prior_hc_data • hcaidcs</title>
 
 <!-- jquery -->
 <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha384-nrOSfDHtoPMzJHjVTdCopGqIqeYETSXhZDFyniQ8ZHcVy08QesyHcnOUpMpqnmWq" crossorigin="anonymous"></script>
@@ -28,9 +28,9 @@
 
 
 
-<meta property="og:title" content="Data for testing assignement algorithm — cdi_prior_hc_data" />
+<meta property="og:title" content="Data for testing prior healthcare exposure monthly table — cdi_prior_hc_data" />
 
-<meta property="og:description" content="A sample data set for testing the assignment_algorithm function." />
+<meta property="og:description" content="A sample data set to test the monthly table preparation for prior healthcare exposure" />
 <meta name="twitter:card" content="summary" />
 
 
@@ -97,14 +97,14 @@
 <div class="row">
   <div class="col-md-9 contents">
     <div class="page-header">
-    <h1>Data for testing assignement algorithm</h1>
+    <h1>Data for testing prior healthcare exposure monthly table</h1>
     <small class="dont-index">Source: <a href='https://github.com/PublicHealthEngland/hcaidcs/blob/master/R/data.R'><code>R/data.R</code></a></small>
     <div class="hidden name"><code>cdi_prior_hc_data.Rd</code></div>
     </div>
 
     <div class="ref-description">
     
-    <p>A sample data set for testing the assignment_algorithm function.</p>
+    <p>A sample data set to test the monthly table preparation for prior healthcare exposure</p>
     
     </div>
 
@@ -112,13 +112,25 @@
         
     <h2 class="hasAnchor" id="format"><a class="anchor" href="#format"></a>Format</h2>
 
-    <p>A data frame with 14 rows of 6 variables:</p><dl class='dl-horizontal'>
- <dt>pircasestatus</dt><dd><p>string</p></dd>
- <dt>assignmentmethodcode</dt><dd><p>Numeric giving assignment method</p></dd>
- <dt>patientlocation</dt><dd><p>string</p></dd>
- <dt>provisionalorganisationname</dt><dd><p>string</p></dd>
- <dt>finalpirassignedorganisation</dt><dd><p>string</p></dd>
- <dt>patient_category</dt><dd><p>string</p></dd>
+    <p>A data frame with 13 rows of 16 variables</p><dl class='dl-horizontal'>
+ <dt>org_code</dt><dd><p>Organisation code (CCG or NHS trust)</p></dd>
+ <dt>year_no</dt><dd><p>Four-digit year</p></dd>
+ <dt>month_no</dt><dd><p>Numeric month</p></dd>
+ <dt>data_collection</dt><dd><p>Data collection</p></dd>
+ <dt>fyear6</dt><dd><p>Financial year</p></dd>
+ <dt>total_cases</dt><dd><p>Count of total cases</p></dd>
+ <dt>apportioned</dt><dd><p>Count of apportioned cases</p></dd>
+ <dt>nhs_trust_pir</dt><dd><p>Count of trust-assigned cases</p></dd>
+ <dt>ccg_pir</dt><dd><p>Count of ccg-assigned cases</p></dd>
+ <dt>third_party_pir</dt><dd><p>Count of third-party cases</p></dd>
+ <dt>hoha</dt><dd><p>Count of hoha cases</p></dd>
+ <dt>coha</dt><dd><p>Count of coha cases</p></dd>
+ <dt>coia</dt><dd><p>Count of coia cases</p></dd>
+ <dt>coca</dt><dd><p>Count of coca cases</p></dd>
+ <dt>phc_all_blank</dt><dd><p>Count of cases for which all prior health care fields are all blank</p></dd>
+ <dt>phc_unknown_3_mo</dt><dd><p>Count of cases for which the three-month prior health care fields is blank</p></dd>
+ <dt>fmonth</dt><dd><p>Numeric financial year month (i.e. 1 = April)</p></dd>
+ <dt>month_string</dt><dd><p>String cal month</p></dd>
 </dl>
     
 

docs/reference/index.html
@@ -132,6 +132,12 @@
       </tr><tr>
         <!--  -->
         <td>
+          <p><code><a href="aec_age_trend_rate_pc_change.html">aec_age_trend_rate_pc_change()</a></code> </p>
+        </td>
+        <td><p>aec_age_trend_rate_pc_change</p></td>
+      </tr><tr>
+        <!--  -->
+        <td>
           <p><code><a href="aec_create_folder_structure.html">aec_create_folder_structure()</a></code> </p>
         </td>
         <td><p>Set up empty folders for Annual publication</p></td>
@@ -421,6 +427,12 @@ Used in construction of file paths to get to raw data and also for printing in t
       </tr><tr>
         <!--  -->
         <td>
+          <p><code><a href="aec_age_trend_rate_pc_change.html">aec_age_trend_rate_pc_change()</a></code> </p>
+        </td>
+        <td><p>aec_age_trend_rate_pc_change</p></td>
+      </tr><tr>
+        <!--  -->
+        <td>
           <p><code><a href="aec_create_folder_structure.html">aec_create_folder_structure()</a></code> </p>
         </td>
         <td><p>Set up empty folders for Annual publication</p></td>
@@ -513,7 +525,7 @@ Used in construction of file paths to get to raw data and also for printing in t
         <td>
           <p><code><a href="assignment_data.html">assignment_data</a></code> </p>
         </td>
-        <td><p>Data for testing prior healthcare exposure monthly table</p></td>
+        <td><p>Data for testing assignement algorithm</p></td>
       </tr><tr>
         <!--  -->
         <td>
@@ -537,7 +549,7 @@ Used in construction of file paths to get to raw data and also for printing in t
         <td>
           <p><code><a href="cdi_prior_hc_data.html">cdi_prior_hc_data</a></code> </p>
         </td>
-        <td><p>Data for testing assignement algorithm</p></td>
+        <td><p>Data for testing prior healthcare exposure monthly table</p></td>
       </tr><tr>
         <!--  -->
         <td>

man/aec_age_trend_rate.Rd
@@ -38,4 +38,6 @@ q
 }
 \seealso{
 \code{\link{aec_age_trend_pc}}
+
+\code{\link{aec_age_trend_rate_pc_change}}
 }

man/aec_age_trend_rate_pc_change.Rd
@@ -1,0 +1,29 @@
+% Generated by roxygen2: do not edit by hand
+% Please edit documentation in R/aec_age_trends_plots.R
+\name{aec_age_trend_rate_pc_change}
+\alias{aec_age_trend_rate_pc_change}
+\title{aec_age_trend_rate_pc_change}
+\usage{
+aec_age_trend_rate_pc_change(data, collection)
+}
+\arguments{
+\item{data}{a dataframe containing the following variables: fyear6, age_group_new, sex, rate}
+
+\item{collection}{A string giving either "CDI" or "bacteraemia". This determines the age groups}
+}
+\value{
+A ggplot2 object
+}
+\description{
+Plot the change in rate as a percentage from first year of surveillance.
+}
+\examples{
+data(age_trends_data)
+p <- aec_age_trend_rate_pc_change(data = age_trends_data, collection = "CDI")
+p
+}
+\seealso{
+\code{\link{aec_age_trend_pc}}
+
+\code{\link{aec_age_trend_rate}}
+}

man/ann_tab_pir_quarterly.Rd
@@ -7,10 +7,33 @@
 ann_tab_pir_quarterly(data, timeperiod, org_code, denominator, total,
   pir_trust_assigned, pir_ccg_assigned, third_party, org_type)
 }
+\arguments{
+\item{data}{A data frame}
+
+\item{timeperiod}{A string giving the time period}
+
+\item{org_code}{A string giving the organisation code}
+
+\item{denominator}{A numeric variable giving the denominator (popn or bed days)}
+
+\item{total}{Count of total cases}
+
+\item{pir_trust_assigned}{Count of PIR trust-assigned cases}
+
+\item{pir_ccg_assigned}{Count of PIR CCG-assigned cases}
+
+\item{third_party}{Count of PIR third-party-assigned cases}
+
+\item{org_type}{String giving trust or CCG}
+}
+\value{
+A data frame spread into format for wide tables.
+}
 \description{
 Prepare quarterly MRSA PIR tables
 }
 \examples{
+
 
 pir_qtr_testdat <- data.frame(stringsAsFactors=FALSE,
     time_period = c("April - June 2013", "July - September 2013",

man/assignment_data.Rd
@@ -3,30 +3,20 @@
 \docType{data}
 \name{assignment_data}
 \alias{assignment_data}
-\title{Data for testing prior healthcare exposure monthly table}
-\format{A data frame with 13 rows of 16 variables
+\title{Data for testing assignement algorithm}
+\format{A data frame with 14 rows of 6 variables:
 \describe{
- \item{org_code}{Organisation code (CCG or NHS trust)}
- \item{year_no}{Four-digit year}
- \item{month_no}{Numeric month}
- \item{data_collection}{Data collection}
- \item{fyear6}{Financial year}
- \item{total_cases}{Count of total cases}
- \item{apportioned}{Count of apportioned cases}
- \item{nhs_trust_pir}{Count of trust-assigned cases}
- \item{ccg_pir}{Count of ccg-assigned cases}
- \item{third_party_pir}{Count of third-party cases}
- \item{hoha}{Count of hoha cases}
- \item{coha}{Count of coha cases}
- \item{coia}{Count of coia cases}
- \item{coca}{Count of coca cases}
- \item{fmonth}{Numeric financial year month (i.e. 1 = April)}
- \item{month_string}{String cal month}
+ \item{pircasestatus}{string}
+ \item{assignmentmethodcode}{Numeric giving assignment method}
+ \item{patientlocation}{string}
+ \item{provisionalorganisationname}{string}
+ \item{finalpirassignedorganisation}{string}
+ \item{patient_category}{string}
 }}
 \usage{
 assignment_data
 }
 \description{
-A sample data set to test the monthly table preparation for prior healthcare exposure
+A sample data set for testing the assignment_algorithm function.
 }
 \keyword{datasets}

man/assignment_data2.Rd
@@ -9,10 +9,10 @@
  \item{pir_case_status}{string}
  \item{assignment_method_code}{Numeric giving assignment method}
  \item{patient_location}{string}
+ \item{patient_category}{string}
  \item{provisional_organisation_name}{string}
  \item{final_pir_assigned_organisation_type}{string}
- \item{patient_category}{string}
- \item{id}{numeric}
+ \item{id}{A numeric variable giving an anonymous HCAIDCS case id}
 }}
 \usage{
 assignment_data2

man/cdi_prior_hc_data.Rd
@@ -3,20 +3,32 @@
 \docType{data}
 \name{cdi_prior_hc_data}
 \alias{cdi_prior_hc_data}
-\title{Data for testing assignement algorithm}
-\format{A data frame with 14 rows of 6 variables:
+\title{Data for testing prior healthcare exposure monthly table}
+\format{A data frame with 13 rows of 16 variables
 \describe{
- \item{pircasestatus}{string}
- \item{assignmentmethodcode}{Numeric giving assignment method}
- \item{patientlocation}{string}
- \item{provisionalorganisationname}{string}
- \item{finalpirassignedorganisation}{string}
- \item{patient_category}{string}
+ \item{org_code}{Organisation code (CCG or NHS trust)}
+ \item{year_no}{Four-digit year}
+ \item{month_no}{Numeric month}
+ \item{data_collection}{Data collection}
+ \item{fyear6}{Financial year}
+ \item{total_cases}{Count of total cases}
+ \item{apportioned}{Count of apportioned cases}
+ \item{nhs_trust_pir}{Count of trust-assigned cases}
+ \item{ccg_pir}{Count of ccg-assigned cases}
+ \item{third_party_pir}{Count of third-party cases}
+ \item{hoha}{Count of hoha cases}
+ \item{coha}{Count of coha cases}
+ \item{coia}{Count of coia cases}
+ \item{coca}{Count of coca cases}
+ \item{phc_all_blank}{Count of cases for which all prior health care fields are all blank}
+ \item{phc_unknown_3_mo}{Count of cases for which the three-month prior health care fields is blank}
+ \item{fmonth}{Numeric financial year month (i.e. 1 = April)}
+ \item{month_string}{String cal month}
 }}
 \usage{
 cdi_prior_hc_data
 }
 \description{
-A sample data set for testing the assignment_algorithm function.
+A sample data set to test the monthly table preparation for prior healthcare exposure
 }
 \keyword{datasets}