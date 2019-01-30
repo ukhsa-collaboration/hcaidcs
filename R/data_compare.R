29c546d4c25f2a2f3ffa6e26865959cfe0004131
Piotr Patrzylas <Piotr.Patrzylas@phe.gov.uk>
2019-01-30 12:49:27 +0000

    Added merge_everything(R/ and tests/) and modified R/data_compare

--- R/data_compare.R
+++ R/data_compare.R
@@ -53,3 +53,43 @@ data_compare <- function(currentdf, lastdf) {
   # Return the matching data.frame that can be used to colour cells:
   return(cells2flag)
 }
+
+# Added function after "original"
+
+# Highligh function - main function for applying red font colour to changes in the workbooks
+highlight <- function(old_file, new_file){
+
+  #Font colour red
+  red_style = openxlsx::createStyle(fontColour =  "#FF0000")
+
+  #Copy of new file, all changes will be highlighted in this file
+  temp_workbook <- openxlsx::loadWorkbook(file = new_file)
+
+  #sheet_number checks how many worksheets is in the file, first worksheet is omitted by default
+  sheet_number <- c(2:length(openxlsx::getSheetNames(old_file)))
+
+  #in this loop for every worksheet ("i") we compare old and new worksheets
+  for (i in sheet_number) {
+
+    #read new/old worksheet, no skipping empty cells for more clear structure of data frames
+    new_sheet <- openxlsx::read.xlsx(xlsxFile = new_file, sheet = i, skipEmptyRows = F, skipEmptyCols = F, colNames = F, rowNames = F)
+    old_sheet <- openxlsx::read.xlsx(xlsxFile = old_file, sheet = i, skipEmptyRows = F, skipEmptyCols = F, colNames = F, rowNames = F)
+
+    #new_cols - we compare all the columns from the old file except the last one which does not contain data
+    new_cols <- (length(old_sheet)-1)
+
+    #trim functions below make sure data frames are of the same length, which means any new columns added to new file will be excluded
+    new_sheettrim <- subset(new_sheet, select = 0:new_cols)
+    old_sheettrim <- subset(old_sheet, select = 0:new_cols)
+
+    #using data_compare function (see above) return rows and columns to be highlighted
+    dcomp <- data_compare(new_sheettrim, old_sheettrim)
+    rows_com <- dcomp[,1]
+    cols_com <- dcomp[,2]
+
+    #apply highlighting to workbook, keep other formatting (stack = T)
+    openxlsx::addStyle(wb = temp_workbook, sheet = i, style = red_style, rows = rows_com, cols = cols_com, stack = T)
+  }
+  #save workbook with highlighted cells
+  openxlsx::saveWorkbook(wb = temp_workbook, file = "highlight_results.xlsx", overwrite = T)
+    }
---
+++ tests/testthat/test-merge_everything.R
@@ -0,0 +1,76 @@
+context("merge_everything")
+
+#Basic setup
+save.image(file = "temp_ge.RData")
+original_dir = getwd()
+library(testthat)
+#library(rstudioapi)
+#current_path = rstudioapi::getActiveDocumentContext()$path
+#current_dir = dirname(current_path)
+current_dir = getwd()
+temp_dir = tempdir()
+file.copy(file.path(current_dir, "test-merge_everything_MRSA.xlsx"), temp_dir, overwrite = T)
+file.copy(file.path(current_dir, "test-merge_everything_NON_MRSA.xlsx"), temp_dir, overwrite = T)
+setwd(temp_dir )
+
+#Temp variables & files
+MRSA_file <- "test-merge_everything_MRSA.xlsx"
+NON_MRSA_file <- "test-merge_everything_NON-MRSA.xlsx"
+trust_list <- list(list(c("RCF", "RFF", "RAE"), "RCF"), list(c("RF4", "R1H"), "R1H"), list(c("RWY", "RJF", "RXQ", "RXH"), "RXH"))
+ccg_list <- list(list(c("07L", "07M", "07N"), "07L"), list(c("00T", "00R", "00Q"), "00Q"), list(c("02N", "02P"), "02P"))
+merge_everything("test-merge_everything_NON_MRSA.xlsx", 1, trust_list, ccg_list, result_file = "NON_MRSA.xlsx")
+merge_everything("test-merge_everything_NON_MRSA.xlsx", 1, result_file = "Error.xlsx")
+merge_everything("test-merge_everything_MRSA.xlsx", 2, trust_list, ccg_list, result_file = "MRSA.xlsx")
+non_mrsa_w2 <- openxlsx::read.xlsx("NON_MRSA.xlsx", sheet = 2, startRow = 1, colNames = TRUE, rowNames = FALSE, skipEmptyRows = FALSE, skipEmptyCols = FALSE)
+non_mrsa_w3 <- openxlsx::read.xlsx("NON_MRSA.xlsx", sheet = 3, startRow = 1, colNames = TRUE, rowNames = FALSE, skipEmptyRows = FALSE, skipEmptyCols = FALSE)
+non_mrsa_w4 <- openxlsx::read.xlsx("NON_MRSA.xlsx", sheet = 4, startRow = 1, colNames = TRUE, rowNames = FALSE, skipEmptyRows = FALSE, skipEmptyCols = FALSE)
+non_mrsa_w5 <- openxlsx::read.xlsx("NON_MRSA.xlsx", sheet = 5, startRow = 1, colNames = TRUE, rowNames = FALSE, skipEmptyRows = FALSE, skipEmptyCols = FALSE)
+mrsa_w3 <- openxlsx::read.xlsx("MRSA.xlsx", sheet = 3, startRow = 1, colNames = TRUE, rowNames = FALSE, skipEmptyRows = FALSE, skipEmptyCols = FALSE)
+mrsa_w4 <- openxlsx::read.xlsx("MRSA.xlsx", sheet = 4, startRow = 1, colNames = TRUE, rowNames = FALSE, skipEmptyRows = FALSE, skipEmptyCols = FALSE)
+
+#Tests - output files
+test_that("Function creates files", {
+  expect_true (file.exists("NON_MRSA.xlsx"), file.exists("MRSA.xlsx"))
+})
+
+test_that("Function does nothing when codes are null", {
+  expect_true(!file.exists("Error.xlsx"))
+})
+
+test_that("Number of worksheets in workbook is correct", {
+  expect_equal(length(getSheetNames("NON_MRSA.xlsx")), 5)
+})
+
+test_that("Correct number of rows/columns in output file", {
+  expect_equal(dim(non_mrsa_w2), c(17, 6))
+})
+#Tests - processed information
+test_that("Cell is correctly calculated for non-MRSA type w2", {
+  expect_equal(as.numeric(non_mrsa_w2[6,3]), 29982)
+})
+
+test_that("Cell is correctly calculated for non-MRSA type w3", {
+  expect_equal(as.numeric(non_mrsa_w3[5,5]), 50)
+})
+
+test_that("Cell is correctly calculated for non-MRSA type w4", {
+  expect_equal(as.numeric(non_mrsa_w4[9,5]), 1995)
+})
+
+test_that("Cell is correctly calculated for non-MRSA type w5", {
+  expect_equal(as.numeric(non_mrsa_w5[9,7]), 22)
+})
+
+test_that("Cell is correctly calculated for MRSA type w3", {
+  expect_equal(as.numeric(mrsa_w3[7,7]), 58)
+})
+
+test_that("Cell is correctly calculated for MRSA type w4", {
+  expect_equal(as.numeric(mrsa_w4[6,6]), 11.5)
+})
+
+setwd(original_dir)
+remove(list = ls())
+invisible(gc())
+load("temp_ge.RData")
+file.remove("temp_ge.RData")
